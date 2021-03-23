25d02966bb65195d78d0877b1bf5e72605ccac48
antoniochirik <antoniochirik@yandex.ru>
2021-03-23 22:20:08 +0300

    remade Review Model

--- artworks/models.py
+++ artworks/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete = models.CASCADE,
+        related_name = 'reviews'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name = 'reviews'
+    )
+    score = models.IntegerField(
+        default=5,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ]
+    )
+    pub_date = models.DateTimeField(
+        autho_now_add=True
+    )
