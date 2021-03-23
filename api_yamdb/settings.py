a553c72516196fd3cab210c7ca0519b273177c09
antoniochirik <antoniochirik@yandex.ru>
2021-03-23 20:50:15 +0300

    add apps in settings

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'api_app',
+    'artworks',
+    'users',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
