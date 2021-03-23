087c90b98f19491a2a67fd84e5c5a375f7581c28
antoniochirik <antoniochirik@yandex.ru>
2021-03-23 19:24:04 +0300

    create review model

--- artworks/models.py
+++ artworks/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+class Review(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name = 'review',
+        blank = True
+    )
+    score = models.IntegerField(
+        default=5,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+        ]
+    )
+    pub_date = models.DateTimeField(
+        autho_now_add=True
+    )
