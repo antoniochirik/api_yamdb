3efa437cd9e6113236d93ba435f67bf8cd2038f0
antoniochirik <antoniochirik@yandex.ru>
2021-03-26 14:12:05 +0300

    permission fixies

--- api_app/permissions.py
+++ api_app/permissions.py
@@ -1,24 +1,19 @@
 from rest_framework import permissions
 
 
-class IsAuthorOrReadOnly(permissions.BasePermission):
+class IsAuthorModeratorAdminOrReadOnly(permissions.BasePermission):
+
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user)
+                or obj.author == request.user
+                or request.user.role == 'moderator'
+                or request.user.role == 'admin')
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
+
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
         else:
             return request.user.is_staff
-
-
-class IsAdmin(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.user.is_anonymous:
-            return False
-
-        if request.user.role == 'admin':
-            return True
