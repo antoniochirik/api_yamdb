e99b4d20a5cc23bcd340909c33f5dfda050b0f17
Anton Emelyanov <emelyaart@gmail.com>
2021-03-26 15:25:25 +0500

    add users (#11)

--- api_app/permissions.py
+++ api_app/permissions.py
@@ -5,11 +5,20 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
-      
-      
+
+
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
         else:
             return request.user.is_staff
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_anonymous:
+            return False
+
+        if request.user.role == 'admin':
+            return True
--- api_app/serializers.py
+++ api_app/serializers.py
@@ -2,9 +2,8 @@ from rest_framework import serializers
 from django.db.models import Avg, F
 from django.contrib.auth import get_user_model
 
-from artworks.models import Comment,Review, Title, Category, Genre
+from artworks.models import Comment, Review, Title, Category, Genre
 User = get_user_model()
-
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -20,43 +19,46 @@ class CategoryField(serializers.SlugRelatedField):
             return self.get_queryset().get(**{self.slug_field: data})
         except (TypeError, ValueError):
             self.fail('invalid')
+
     def to_representation(self, value):
         return CategorySerializer(value).data
 
-      
+
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         lookup_field = 'slug'
         exclude = ['id']
         model = Genre
-        
-        
+
+
 class GenreField(serializers.SlugRelatedField):
     def to_internal_value(self, data):
         try:
             return self.get_queryset().get(**{self.slug_field: data})
         except (TypeError, ValueError):
             self.fail('invalid')
+
     def to_representation(self, value):
         return GenreSerializer(value).data
-      
-      
+
+
 class TitleSerializer(serializers.ModelSerializer):
     genre = GenreField(
         many=True,
         slug_field='slug',
         queryset=Genre.objects.all()
+    )
 
-      
     category = CategoryField(
         slug_field='slug',
         queryset=Category.objects.all()
+    )
 
     class Meta:
         fields = '__all__'
         model = Title
-        
-        
+
+
 class ReviewSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         many=False,
@@ -68,11 +70,12 @@ class ReviewSerializer(serializers.ModelSerializer):
         read_only=True,
         slug_field='id'
     )
+
     class Meta:
         model = Review
         fields = '__all__'
 
-      
+
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         many=False,
@@ -84,11 +87,12 @@ class CommentSerializer(serializers.ModelSerializer):
         read_only=True,
         slug_field='id'
     )
+
     class Meta:
         model = Comment
         fields = '__all__'
-        
-        
+
+
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -100,8 +104,8 @@ class CustomUserSerializer(serializers.ModelSerializer):
             'email',
             'role'
         )
-        
-        
+
+
 class UsernameSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -113,8 +117,8 @@ class UsernameSerializer(serializers.ModelSerializer):
             'email',
             'role'
         )
-        
-        
+
+
 class UserAPIViewSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
